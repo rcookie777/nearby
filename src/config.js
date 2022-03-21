94f0256f2fbb2282a148a6caba913de5f3d0ca32
Riley cook <cookril2@msu.edu>
2022-03-20 22:02:02 -0400

    Add Contract Page

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const CONTRACT_NAME = process.env.CONTRACT_NAME ||'nearby.rgc.testnet'
+const CONTRACT_NAME = process.env.CONTRACT_NAME ||'spartans.testnet'
 
 function getConfig(env) {
   switch (env) {
